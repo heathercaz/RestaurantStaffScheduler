b0115ccc576f6b5e7903db777af39d058dd5435d
heathercaz <heather.caswell14@gmail.com>
2025-07-20 13:36:33 -0600

    Initial commit of front-end file

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Welcome to the Restaurant Staff Scheduler</h1>
+      <p>Please use the navigation to manage staff members and shifts.</p>
+    </div>
+  );
+}
+
+export default App;
